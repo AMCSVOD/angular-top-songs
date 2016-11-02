89bb13152a592be49e1187a2c0da22296370ee69
Suri <awumsuri@gmail.com>
2016-11-02 19:31:39 -0400

    Expanded artists

---
+++ app/controllers/PlaylistController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Mtui on 11/2/16.
+ */
+"use strict";
+
+(function() {
+    angular.module("MusicApp").controller("Playlist", ["$scope", "Spotify", "$rootScope", "$window", "Configs", function($scope, Spotify, $rootScope, Configs, $window) {
+
+    }])
+})();
+
+
--- build/controllers.js
+++ build/controllers.js
@@ -27,27 +27,62 @@
 "use strict";
 
 (function() {
-    angular.module("MusicApp").controller("HomeController", ["$scope", "Spotify", "$rootScope", "$window", "Configs", function($scope, Spotify, $rootScope, Configs, $window) {
-
-        var displayData = null;
+    angular.module("MusicApp").controller("HomeController", ["$scope", "Spotify", "$rootScope", function($scope, Spotify, $rootScope) {
 
         if ($rootScope.user === undefined) {
             window.location.href = "/";
+        }
+
+        var type = null;
+
+        $scope.displayData = [];
+        $scope.playlist = [];
+
+        $scope.itemClickHandler = function(item) {
+            debugger;
         }
 
         $scope.submit = function() {
             var searchTerm = $scope.searchTerm;
 
             if(searchTerm !== '') {
-                Spotify.search(searchTerm, getType()).then(function (data) {
-                    console.log(data);
+                type = getType();
+
+                Spotify.search(searchTerm, type).then(function (data) {
+                    populateView(data);
                 });
             }
         };
 
+        function populateView(data) {
+            var tempData = data[type+'s'].items;
+
+            tempData.forEach(function(tempItem) {
+                var names = Array.prototype.map.call(tempItem.artists, function(artist) {
+                    return artist.name;
+                });
+
+                tempItem.finalArtists = names.join(", ");
+            });
+
+            $scope.displayData = tempData;
+        }
+
         function getType() {
             return document.getElementById("searchType").value;
         }
-
     }]);
 })();
+
+/**
+ * Created by Mtui on 11/2/16.
+ */
+"use strict";
+
+(function() {
+    angular.module("MusicApp").controller("Playlist", ["$scope", "Spotify", "$rootScope", "$window", "Configs", function($scope, Spotify, $rootScope, Configs, $window) {
+
+    }])
+})();
+
+
