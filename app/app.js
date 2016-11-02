939066aaccc102773d091b8eda13b7762bd3042c
Suri <awumsuri@gmail.com>
2016-11-02 01:15:30 -0400

    Finally working initilaztion

---
+++ app/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('MusicApp', ['ngRoute']);
+
+app.controller("MusicController", function($scope) {
+    
+})
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'app/views/AuthView.html',
+            controller: 'app/controllers/AuthController.js'
+        })
+        .when('/test',{
+            template: '<h1>Test</h1><a href="#/">Back</a>'
+        })
+        .otherwise({
+            template: '<h1>Not Found</h1>'
+        });
+});
+
---
+++ app/controllers/AuthController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Mtui on 11/1/16.
+ */
+angular.module("MusicApp").controller("AuthController", ["$scope", function($scope) {
+
+}]);
+
+
